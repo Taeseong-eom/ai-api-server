0d8ca5b6418b4718907e68ee59ecf422bdc19b5d
엄태성 <62461364+Taeseong-eom@users.noreply.github.com>
2025-02-20 02:48:01 +0000

    Rest Client

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Union
+from fastapi import FastAPI
+
+import model # model.py 불러오기
+
+model = model.AndModel() # AndModel 클래스 인스턴스 생성
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/predict/left/{left}/right/{right}")
+def predict(left:int, right:int):
+    result = model.predict([left, right])
+    return {"result" : result}
+
+@app.post("/train")
+def train():
+    model.train()
+    return {"result": "OK"}
+
